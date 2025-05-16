dd56fa6c6d9ba0b4a398b827204da68ac5855f6d
liuyilong <redtea25@qq.com>
2025-05-16 14:26:02 +0800

    EffectScope、onScopeDispose示例

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,61 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <button @click="startCounter">开始计数</button>
+    <button @click="stopCounter" :disabled="!isRunning">停止计数</button>
+    <p>计数器: {{ counter }}</p>
+    <p>计算值: {{ doubled }}</p>
+    <p v-if="isRunning">作用域状态: 运行中</p>
+    <p v-else>作用域状态: 已停止</p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script setup lang="ts">
+import { ref, computed, effectScope, watch, onScopeDispose, EffectScope } from 'vue'
+
+const counter = ref(0)
+const isRunning = ref(false)
+let scope: EffectScope | null = null
+const doubled = ref(0)
+
+// 开始计数器
+const startCounter = () => {
+  if (scope) scope.stop()
+
+  scope = effectScope()
+  scope.run(() => {
+    const interval = setInterval(() => {
+      counter.value++
+    }, 1000)
+
+    const doubledComputed = computed(() => counter.value * 2)
+    watch(
+      doubledComputed,
+      (val) => {
+        doubled.value = val
+      },
+      { immediate: true },
+    )
+
+    watch(counter, (newValue) => {
+      console.log('计数器更新为:', newValue)
+    })
+
+    isRunning.value = true
+
+    // 正确的清理方式
+    onScopeDispose(() => {
+      clearInterval(interval)
+      isRunning.value = false
+      console.log('作用域已停止')
+    })
+  })
+}
+
+// 停止计数器
+const stopCounter = () => {
+  if (scope) {
+    scope.stop()
+    scope = null
   }
 }
-</style>
+</script>
